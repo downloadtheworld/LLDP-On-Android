e62bf1a1521e96e992ab0eba4bf5e450df7ec13f
downloadtheworld <knelmes@gmail.com>
2020-07-28 18:22:46 +0100

    Update get-files.sh

--- get-files.sh
+++ get-files.sh
@@ -1,1 +1,4 @@
 curl https://www.androidtcpdump.com/download/4.9.3.1.9.1/tcpdump tcpdump
+curl https://raw.githubusercontent.com/downloadtheworld/LLDP-On-Android/master/mon.sh
+chmod +x tcpdump
+chmod +x mon.sh
