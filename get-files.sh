568801d6d7d7ea26d8158de219d499d1dba8eb8d
downloadtheworld <knelmes@gmail.com>
2022-08-05 20:31:25 +0100

    Change URL to latest tcpdump version

--- get-files.sh
+++ get-files.sh
@@ -1,4 +1,4 @@
-curl -O https://www.androidtcpdump.com/download/4.9.3.1.9.1/tcpdump
+curl -O https://www.androidtcpdump.com/download/4.99.1.1.10.1/tcpdump
 curl -O https://raw.githubusercontent.com/downloadtheworld/LLDP-On-Android/master/mon.sh
 chmod +x tcpdump
 chmod +x mon.sh
